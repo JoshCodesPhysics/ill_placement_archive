1111b3ac222d5c3975793b18c03785adc2c0dd90
Joshua Horswill <horswill@ill.fr>
2020-07-07 16:44:49 +0100

    more comments

--- born_tensor_script/bt_extract.py
+++ born_tensor_script/bt_extract.py
@@ -23,9 +23,11 @@ def num_atoms(born_file):
 def matrix_data(born_file):
     "Stores matrix data for each atom as 3x3 matrix corresponding to the atom key"
     atom_num = num_atoms(born_file)
+    #Generating atom data and empty storage matrix
     atom_array = np.arange(1,atom_num+1,1)
     matrix_dict = {"atom%d"%i:np.zeros((3,3)) for i in atom_array}
     born = open(born_file).readlines()
+    #Searching for title and truncating relevant data
     for i in range(len(born)):
         born_split = born[i].split()
         if born_split[:3] == ['BORN', 'CHARGE', 'TENSOR.']:
@@ -33,6 +35,7 @@ def matrix_data(born_file):
             end = i+1 + (7*atom_num)
             break
     tensor_data = born[start:end]
+    #Appending data to dictionary as 3x3 matrices
     for i in range(len(tensor_data)):
         tensorsplit = tensor_data[i].split()
         if len(tensorsplit) > 0:
@@ -56,6 +59,7 @@ def born_tensor(born_file):
     tensor = np.zeros((m_dim,m_dim))
     start_column = 0
     atom_num = 1
+    #Looping through tensor columns and adding stepped matrices along the diagonal
     for i in range(m_dim):
         if i != 0 and i%3 ==0:
             start_column += 3
@@ -70,26 +74,37 @@ def parse_hessian(hess_file,thresh):
     hess = open(hess_file).readlines()
     for i in range(len(hess)):
         hess_split = hess[i].split()
+        #Appending all values to hess_data from input file, unless the value is very small, then it is appended as zero
         for j in range(len(hess_split)):
-            if float(hess_split[j])<= 1e-15:
+            if abs(float(hess_split[j]))<= 1e-15:
                 hess_data.append(0.0)
             else:
+                #Appending data and keeping the original index for later
                 hess_data.append((float(hess_split[j]),i*len(hess_split)+j))
-    hess_copy = copy.deepcopy(hess_data) 
+    #Keeping a copy of the data with zeros included
+    hess_copy = copy.deepcopy(hess_data)
+    #Removing zeros
     hess_data = [i for i in hess_data if i != 0.0]
     hess_pairs = []
     hess_diag = []
     flag = True
+    #While there are still elements in hess_data, append and remove pairs
+    #with their respective indexes as well as unique elements with their indexes.
     while flag == True:
+        #Uncomment below statement if you want a 'progress bar'
         #print("initial element length ",len(hess_data),"twice pair length + diag length ",2*len(hess_pairs)+len(hess_diag))
         if len(hess_data) == 1:
+            #Removes bug for last element
             hess_diag.append(hess_data[0])
             hess_data.remove(hess_data[0])
             break
+        #Break once list is empty
         if len(hess_data) == 0:
             flag = False
             break
         for i in range(1,len(hess_data)):
+            #If the difference between two elements satisfies the threshold
+            #Add them and their indexes to a pair list and remove both from hess_data
             if abs(hess_data[0][0]-hess_data[i][0]) <= thresh:
                 mean = (hess_data[0][0]+hess_data[i][0])/2.0
                 hess_pairs.append([mean,hess_data[0][1],hess_data[i][1]])
@@ -97,9 +112,11 @@ def parse_hessian(hess_file,thresh):
                 rem2 = hess_data[i]
                 hess_data.remove(rem1),hess_data.remove(rem2)
                 break
+            #If there are no pairs for this value, add it to diagonal list with it's index and remove
             elif i == len(hess_data)-1:
                 hess_diag.append([hess_data[0][0],hess_data[0][1]])
                 hess_data.remove(hess_data[0])
+    #return triple of pairs, diagonal and original data lists
     return hess_pairs,hess_diag,hess_copy
 
 def hessian(born_file,hess_file,thresh):
@@ -120,6 +137,7 @@ def solve_equation(born_file,hess_file,thresh,ex,ey,ez):
     h_tensor = np.random.rand(m_dim,m_dim)
     q_tensor = np.random.rand(m_dim,m_dim)
     e_vector = np.ones((m_dim))
+    #Generating e_vector from inputs
     for i in range(len(e_vector)):
         if i % 3 == 0:
             e_vector[i] = ex
@@ -127,9 +145,11 @@ def solve_equation(born_file,hess_file,thresh,ex,ey,ez):
             e_vector[i] = ey
         else:
             e_vector[i] = ez
+    #Generating matrices for linalg solve
     a = -1*h_tensor
     b = q_tensor.dot(e_vector)
     displacement = np.linalg.solve(a,b)
+    #Printing components of linear equation including the solution
     print("Negative Hessian: ")
     print(a)
     print("Born tensor: ")
@@ -140,40 +160,47 @@ def solve_equation(born_file,hess_file,thresh,ex,ey,ez):
     print(displacement)
 
 def print_indexes(hess_file,thresh):
-    "Prints all paired and unique values with their frequencies"
+    "Prints all paired and unique values with their indexes"
     print("Pair indexes")
     print([i[1:] for i in parse_hessian(hess_file,thresh)[0]])
     print("Diag indexes")
     print([i[1] for i in parse_hessian(hess_file,thresh)[1]])
 
 def print_diag(hess_file,thresh,rev):
-    """Prints diagonal elements in either ascending
+    """Writes diagonal elements to a text file in either ascending
     or descending order depending on rev boolean"""
     with open("diagonal.txt",'w') as diag:
         sort_diag = parse_hessian(hess_file,thresh)[1]
         sort_diag = [i[0] for i in sort_diag]
+        #Sorting ascending or descending, rev = True creates descending
         sort_diag.sort(reverse=rev)
         print("Length ", len(sort_diag))
         for item in sort_diag:
             diag.write("%s\n"%str(item))
 
-def num_diag(hess_file):
-    thresh_range = np.ones((15))
+def num_diag(hess_file,nthresh,threshstart):
+    "Prints a dictionary containing number of diagonal elements for a range of thresholds"
+    "nthresh gives number of thresholds to test, and threshstart gives starting value"
+    thresh_range = np.ones((nthresh))
+    #Generating threshold values
     for i in range(len(thresh_range)):
         if i == 0:
-            thresh_range[i] = 1e-3
+            thresh_range[i] = threshstart
         else:
+            #One order of magnitude between each index
             thresh_range[i] = 1e-1*thresh_range[i-1]
+    #Empty dictionary for writing
     diag_dict = {str(i):None for i in thresh_range}
+    #Loop writing all diagonal list lengths for given thresholds
     for i in range(len(thresh_range)):
         diag_dict['%s'%str(thresh_range[i])] = len(parse_hessian("Pm.rePhonons.2123667.HESSFREQ.DAT",thresh_range[i])[1])
     print("Format is 'threshold':number of diagonal elements")
     print(diag_dict)
 
-num_diag("Pm.rePhonons.2123667.HESSFREQ.DAT")
+num_diag("Pm.rePhonons.2123667.HESSFREQ.DAT",5,1e-3)
 #solve_equation('frequence.B1PW_PtBs.loto.out',"Pm.rePhonons.2123667.HESSFREQ.DAT",1,2,3)
-#print_diag("Pm.rePhonons.2123667.HESSFREQ.DAT",1e-4,True)
-#print_indexes("Pm.rePhonons.2123667.HESSFREQ.DAT")
+#print_diag("Pm.rePhonons.2123667.HESSFREQ.DAT",1e-12,True)
+#print_indexes("Pm.rePhonons.2123667.HESSFREQ.DAT",1e-4)
 #hessian("Pcab.reOptGeom.364624.out","Pcab.reOptGeom.364624.HESSOPT.DAT")
 #tensor = born_tensor('frequence.B1PW_PtBs.loto.out')
 #print("Born tensor: ",tensor)
